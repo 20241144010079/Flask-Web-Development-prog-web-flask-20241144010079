4b5a75c50fa7d595bb5f7a8baf58ffecaf10434b
20241144010079 <cristina.elizabeth@escolar.ifrn.edu.br>
2026-07-13 16:39:18 +0000

    git add . git commit -m "fix: configure application factory #2" git push

--- flask-tutorial/__init__.py
+++ flask-tutorial/__init__.py
@@ -6,15 +6,20 @@ from flask import Flask
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
-        app.config.from_mapping(
-                SECRET_KEY='dev',
-                        DATABASE=os.path.join(app.instance_path, 'cores.sqlite'),
-                            )
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'cores.sqlite'),
+    )
 
-                                os.makedirs(app.instance_path, exist_ok=True)
+    os.makedirs(app.instance_path, exist_ok=True)
 
-                                    from . import db
-                                        db.init_app(app)
-from . import cores
-app.register_blueprint(cores.bp)
-                                            return app
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import cores
+    app.register_blueprint(cores.bp)
+
+    return app
